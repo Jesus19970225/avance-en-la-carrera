0a43876c5cf0f6b5553562ec718780ab4945c499
Jesus Gomez <gomezgiljesusdavid704@hotmail.com>
2021-05-01 10:03:48 -0500

    abances en el curso de Programacion Orientada a Objetos

--- Curso de Programación Orientada a Objetos POO/PHP/Car.php
+++ Curso de Programación Orientada a Objetos POO/PHP/Car.php
@@ -1,7 +1,18 @@
 <?php
+require_once('account.php')
 class Car {
-public $id = integer;
-public $license = string;
-public $driver = string;
-public $passengers = integer;
+    public $id;
+    public $license;
+    public $driver;
+    public $passengers;
+
+    public function _construct($license, $driver){
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+
+    public function PrintDataCar(){
+        echo "license: $this->license, conductor:{$this->driver->name}, doument:{$this->driver->documet}";
+    }
 }
+?>
--- Curso de Programación Orientada a Objetos POO/Python/main.py
+++ Curso de Programación Orientada a Objetos POO/Python/main.py
@@ -1,5 +1,15 @@
+from car import Car
+from account import Account
+
+
 def run():
     print("Hola mundo")
+
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
+    print(vars(car))
+
+    car2 = Car("QWE567", Account("Andres Herrera", "ANDA876"))
+    print(vars(car2))
 
 
 if __name__ == '__main__':
