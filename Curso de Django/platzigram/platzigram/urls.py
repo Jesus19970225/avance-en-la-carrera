9e25e8b562302f5fcf33ea668e0a5af51f1cd763
Jesus Gomez <gomezgiljesusdavid704@hotmail.com>
2021-08-07 23:30:53 -0500

    Vistas en Django

--- Curso de Django/platzigram/platzigram/urls.py
+++ Curso de Django/platzigram/platzigram/urls.py
@@ -1,16 +1,15 @@
 """Platzigram URLs module."""
 
 from django.urls import path
-from django.http import HttpResponse
-
-
-def hello_world(request):
-    """Return a greeting."""
-    return HttpResponse('Hello world!')
+from platzigram import views as local_views
+from posts import views as posts_views
 
 
 urlpatterns = [
 
-    path('hello-world/', hello_world)
+    path('hello-world/', local_views.hello_world),
+    path('sorted/', local_views.sort_integers),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
 
+    path('posts/', posts_views.list_posts)
 ]
