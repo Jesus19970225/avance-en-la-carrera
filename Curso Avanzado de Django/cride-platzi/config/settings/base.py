8eb934e5688cbe4b85e0a169e94b065805296d16
Jesus Gomez <gomezgiljesusdavid704@hotmail.com>
2021-10-17 00:48:36 -0500

    avanzando en el curso avanzado de django, hasta verificar cuenta usando JWT

--- Curso Avanzado de Django/cride-platzi/config/settings/base.py
+++ Curso Avanzado de Django/cride-platzi/config/settings/base.py
@@ -44,7 +44,8 @@ DJANGO_APPS = [
 ]
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 THIRD_PARTY_APPS = [
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 LOCAL_APPS = [
     'cride.users.apps.UsersAppConfig',
