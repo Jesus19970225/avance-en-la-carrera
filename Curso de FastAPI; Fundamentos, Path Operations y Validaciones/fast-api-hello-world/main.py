a33d50e793cd0ea82d3f731edcc438f60f98a5b0
Jesus Gomez <gomezgiljesusdavid704@hotmail.com>
2021-11-08 17:36:48 -0500

    validations_body

--- Curso de FastAPI; Fundamentos, Path Operations y Validaciones/fast-api-hello-world/main.py
+++ Curso de FastAPI; Fundamentos, Path Operations y Validaciones/fast-api-hello-world/main.py
@@ -11,6 +11,11 @@ app = FastAPI()
 
 # Models
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -55,7 +60,24 @@ def show_person(
         ...,
         gt=0,
         title="Person id",
-        description="This is the person id. It's greater that 0"
+        description="This is the person ID. It's greater that 0"
         )
 ):
     return {person_id: "It exists"}
+
+# Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
