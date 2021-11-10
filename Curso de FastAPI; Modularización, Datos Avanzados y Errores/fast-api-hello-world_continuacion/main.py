cca216c22de0cac4b5628a7c8bc1626292b5d3ed
Jesus Gomez <gomezgiljesusdavid704@hotmail.com>
2021-11-10 08:15:30 -0500

    response_model

---
+++ Curso de FastAPI; Modularización, Datos Avanzados y Errores/fast-api-hello-world_continuacion/main.py
@@ -0,0 +1,175 @@
+# Python
+from typing import Optional
+from enum import Enum
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic.networks import EmailStr
+
+# FastAPI
+from fastapi import FastAPI, Body, Query, Path
+
+app = FastAPI()
+
+# Models
+
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
+
+class Location(BaseModel):
+    city: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Bogota"
+        )
+    state: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Bogota.D.C"
+        )
+    country: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Colombia"
+        )
+
+class Person(BaseModel):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Miguel"
+        )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Torres"
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=120,
+        example=25
+        )
+    email: EmailStr = Field(
+        ...,
+        example="lolito@hotdog.com"
+        )
+    hair_color: Optional[HairColor] = Field(default=None, example="black")
+    is_married: Optional[bool] = Field(default=None, example=False)
+    password: str = Field(
+        ...,
+        min_length=8,
+        example="holasoymiguel"
+        )
+
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "first_name": "Facundo",
+    #             "last_name": "Garcia Martoni",
+    #             "age": 21,
+    #             "email": "facundo@platzi.com",
+    #             "hair_color": "blonde",
+    #             "is_married": False
+    #         }
+    #     }
+
+class PersonOut(BaseModel):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Miguel"
+        )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Torres"
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=120,
+        example=25
+        )
+    email: EmailStr = Field(
+        ...,
+        example="lolito@hotdog.com"
+        )
+    hair_color: Optional[HairColor] = Field(default=None, example="black")
+    is_married: Optional[bool] = Field(default=None, example=False)
+
+@app.get("/")
+def home():
+    return {"hello": "world"}
+
+# Request and Response Body
+
+@app.post("/person/new", response_model=PersonOut)
+def create_person(person: Person = Body(...)):
+    return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Persone Name",
+        description="This is the person name. It's between 1 and 50 characters",
+        example="Rocio"
+        ),
+    age: int = Query(
+        ...,
+        title="Person Age",
+        description="This is the person age. It's required",
+        example=25
+        )
+):
+    return {name: age}
+
+# Validaciones: Path Parameters
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person id",
+        description="This is the person ID. It's greater that 0",
+        example=123
+        )
+):
+    return {person_id: "It exists"}
+
+# Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0,
+        example=123
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
+    
