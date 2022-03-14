d196955a6d2678e6268d75f13a36a8310c664a6f
Jesus Gomez <gomezgiljesusdavid704@hotmail.com>
2022-03-14 11:56:18 -0500

    Solucionando el error encontrado

--- Curso de Django Intermedio Testing, Static Files, Django Admin/premiosplatzi/premiosplatziapp/polls/tests.py
+++ Curso de Django Intermedio Testing, Static Files, Django Admin/premiosplatzi/premiosplatziapp/polls/tests.py
@@ -15,3 +15,15 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?", pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_present_questions(self):
+        """was_published_recently returns False for questions whose pub_date is the present"""
+        time = timezone.now() 
+        present_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?", pub_date=time)
+        self.assertIs(present_question.was_published_recently(), True)
+
+    def test_was_published_recently_with_past_questions(self):
+        """was_published_recently returns False for questions whose pub_date is the past"""
+        time = timezone.now() - datetime.timedelta(days=1.0001)
+        past_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?", pub_date=time)
+        self.assertIs(past_question.was_published_recently(), False)
